bdf4354383588a8d5afcd93daea62949920e4b12
Sypher0Dronzer <sohamsaha321@gmail.com>
2026-02-02 22:39:28 +0530

    src image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div id='home' className='bg-[url("./hero2.png")] bg-no-repeat bg-cover bg-center h-screen pt-16  relative'>
+    <div id='home' className='bg-[url("../src/assets/hero2.png")] bg-no-repeat bg-cover bg-center h-screen pt-16  relative'>
         <div className="absolute w-full h-full bg-gradient-to-r from-black/5 via-black/20 to-black/5 z-10 top-0 left-0 flex flex-col items-center justify-center">
       <h1 className='hero-header text-8xl tracking-wide lg:text-[170px] char-border'>Bali </h1>
       <h1 className='hero-header text-6xl tracking-wide lg:text-9xl char-border'>Getaway </h1>
